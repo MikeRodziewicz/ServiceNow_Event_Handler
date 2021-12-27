de56b4319429151e9c48dd339d6041162742b0bb
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-12-27 21:07:41 +0100

    more tests and fixed bug in tests

--- src/tests/test_incidents.py
+++ src/tests/test_incidents.py
@@ -10,10 +10,21 @@ class IncidentTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         load_dotenv()
-        self.base_url = os.getenv('BASE_URL')
+        self.base_url = os.getenv('SNOW_URL')
         self.snow_usr = os.getenv('SNOW_USR')
         self.snow_pwd = os.getenv('SNOW_PWD')
         self.connection = BasicSnowConnection(self.base_url, self.snow_usr, self.snow_pwd)
 
     def test_creation_connection_obj(self):
         self.assertIsInstance(self.connection,BasicSnowConnection)
+
+    def test_getting_single_incident(self):
+        test_inc_number = 'INC0008111'
+        response = self.connection.get_single_incident(test_inc_number)
+
+        self.assertNotEqual(response['result'], [])
+        self.assertIn(test_inc_number,response['result'][0]['number'])
+
+    def test_getting_multiple_incidents(self):
+        response = self.connection.get_multiple_emails(sysparm_limit=5)
+        pass
